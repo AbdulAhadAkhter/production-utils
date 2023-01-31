8c56045f6ef28d150d514e7ffdffc972c674ed95
Abdul Ahad Akhter <abdulahadakhter@outlook.com>
2023-01-31 11:45:07 -0500

    Fix script to accept regex instead

--- date.sh
+++ date.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+file_types_regex="\.([cC[rR]2|[dD][nN][gG]|[pP][nN][gG]|[jJ][pP][eE][gG]|[jJ][pP][gG]|[wW][aA][vV])|[mM][pP]4)$"
 
-file_types=(png PNG jpeg JPEG avi AVI mp4 MP4 mp3 MP3 wav WAV)
+# file_types=(png PNG jpeg JPEG avi AVI mp4 MP4 mp3 MP3 wav WAV)
 for file in *; do
     echo "$file"
   if [ -f "$file" ]; then
     ext="${file##*.}"
-    if [[ " ${file_types[@]} " =~ " $ext " ]]; then
+    if [[ "$file" =~ $file_types_regex ]]; then
       date=$(stat -f "%Sm" -t "%Y-%m-%d" "$file" | awk '{print $1}' | tr -d "-")
       if [ ! -d "$date" ]; then
         mkdir "$date"
